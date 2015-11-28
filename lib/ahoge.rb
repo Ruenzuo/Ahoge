fbadb8e7bac3c1abc3077d15945ba29076dd9a27
Renzo Crisóstomo <ruenzuo@gmail.com>
2015-11-28 23:19:30 +0100

    Implements client setup

--- lib/ahoge.rb
+++ lib/ahoge.rb
@@ -1,8 +1,24 @@
+require 'twitter'
+require 'yaml'
+
 module Ahoge
   class Main
+    attr_reader :client
     
     def self.amuse
-   	  puts 'Hello world'
+      main = Main.new
+      main.setup_client
+      main.client.update('Whoops!')
+    end
+
+    def setup_client
+      auth = YAML.load_file(File.join(File.dirname(__FILE__), '../auth.yaml'))
+      @client = Twitter::REST::Client.new do |config|
+        config.consumer_key = auth['consumer_key']
+        config.consumer_secret  = auth['consumer_secret']
+        config.access_token = auth['access_token']
+        config.access_token_secret = auth['access_token_secret']
+      end
     end
   end
 end
