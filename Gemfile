fb34710560412f3634a1ac3441f4bf232aaacd10
Renzo Crisóstomo <ruenzuo@gmail.com>
2015-11-29 17:06:40 +0100

    Implements image merging and caption

--- Gemfile
+++ Gemfile
@@ -4,3 +4,4 @@ ruby '2.0.0'
 
 gem 'twitter', '~> 5.15.0'
 gem 'sqlite3', '~> 1.3.0'
+gem 'mini_magick', '~> 4.3.0'
--- lib/ahoge.rb
+++ lib/ahoge.rb
@@ -1,6 +1,7 @@
 require 'twitter'
 require 'yaml'
 require 'sqlite3'
+require 'mini_magick'
 
 module Ahoge
   class Main
@@ -13,6 +14,8 @@ module Ahoge
       tweet_text, media_url = main.get_last_follower_last_photo
       main.store_media_url(media_url)
       tweet_summarized = main.summarize(tweet_text)
+      main.magic(tweet_summarized, media_url)
+      main.tweet(tweet_summarized, File.new("tweet.png"))
     end
 
     def setup_client
@@ -23,6 +26,10 @@ module Ahoge
         config.access_token = auth['access_token']
         config.access_token_secret = auth['access_token_secret']
       end
+    end
+
+    def tweet(tweet_text, file)
+      @client.update_with_media(tweet_text, file)
     end
 
     def setup_database
@@ -57,7 +64,7 @@ module Ahoge
       tweets.each { |tweet|
         next unless tweet.media?
         tweet.media.each { |media|
-          return [tweet.text, media.media_url] if media.is_a?(Twitter::Media::Photo) and valid_media?(media)
+          return [tweet.text, media.media_url.to_s] if media.is_a?(Twitter::Media::Photo) and valid_media?(media)
         }
       }
       return []
@@ -71,7 +78,7 @@ module Ahoge
     end
 
     def store_media_url(media_url)
-      @db.execute('INSERT INTO media_urls (media_url) VALUES (?)', [media_url.to_s])
+      @db.execute('INSERT INTO media_urls (media_url) VALUES (?)', [media_url])
     end
 
     def summarize(tweet_text)
@@ -80,6 +87,34 @@ module Ahoge
       limit = tokens.count - 2
       random = rand(3..limit)
       return "#{tokens[random - 1]} #{tokens[random]} #{tokens[random + 1]}"
+    end
+
+    def magic(tweet_text, media_url)
+      image = MiniMagick::Image.open(media_url)
+      frame = MiniMagick::Image.open('frame.png')
+      frame.resize("#{image.width}x#{image.height}")
+      anchor = image.width - frame.width
+      result = image.composite(frame) do |c|
+        c.compose 'Over'
+        c.geometry "+#{anchor}+0"
+      end
+      text_anchor = image.height / 4
+      result.combine_options do |c|
+        c.font 'helvetica'
+        c.gravity 'Center'
+        c.stroke 'black'
+        c.strokewidth 4
+        c.pointsize '50'
+        c.draw "text 0,#{text_anchor} '#{tweet_text}'"
+      end
+      result.combine_options do |c|
+        c.font 'helvetica'
+        c.gravity 'Center'
+        c.fill 'white'
+        c.pointsize '50'
+        c.draw "text 0,#{text_anchor} '#{tweet_text}'"
+      end
+      result.write 'tweet.png'
     end
   end
 end
